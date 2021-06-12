9e56cf60d829b5479bac8084463409dc6314bb93
Joana Brito <joanafiorentini@hotmail.com>
2021-06-12 14:05:52 -0700

    Create file

---
+++ main.js
@@ -0,0 +1,12 @@
+// file that contains all DOM related JavaScript
+// This workflow is not required, but will help you meet the overall requirements of the project.
+//
+// Plan out the HTML layout (colors and icons do not need to match, but overall layout should closely match the demo video)
+// Create the Player class
+// Create the Game class
+// Make game fully playable without the DOM (manually updating the Game.board data, etc, from your console) to force yourself to think data-model-first
+// Create central game board on the DOM
+// Connect Game data model to the DOM
+// Display the Player data in the sidebars
+// Automatically reset the game board to allow for a new game to be played after the previous game is won
+// Persist Player data using local storage (number of wins should persist across page refreshes)
