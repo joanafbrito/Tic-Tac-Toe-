36d757942bf74c568d3ad99f1b8f6900eee6e107
Joana Brito <joanafiorentini@hotmail.com>
2021-06-13 15:25:27 -0700

    Add winner method

--- game.js
+++ game.js
@@ -2,15 +2,71 @@ class Game {
   constructor() {
     this.playerOne = new Player("player 1","star");
     this.playerTwo = new Player("player 2","heart");
+    this.currentPlayer = this.playerOne;
     this.board = new Array(9);
+    this.winningMoves = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
+                         [0, 3, 6], [1, 4, 7], [2, 5, 8],
+                         [2, 4, 6], [0, 4, 8]];
 
   }
-  getCurrentPlayer(starter) {
-    if(starter === 1) {
-      this.currentPlayer = this.playerOne;
-    } else {
+  runPlayerClick(clickedBtnIndex) {
+    // o index do botao clicado, vai ser o index da board- mesmo lugar
+    // e nesse index vai ser passado o valor do current player name?
+    this.board[clickedBtnIndex] = this.currentPlayer.name;
+
+
+    if(this.currentPlayer === this.playerOne) {
       this.currentPlayer = this.playerTwo;
+    } else {
+      this.currentPlayer = this.playerOne;
     };
+  }
+  addCurrentPlayerMoves() {
+    var currentPlayerMoves = [];
+
+    for(var i = 0; i < this.board.length; i++) {
+      var playerMove = this.board[i];
+
+      if(playerMove === this.currentPlayer.name) {
+        currentPlayerMoves.push(i);
+      }
+    }
+    return currentPlayerMoves;
+    // is returning one array with the moves of
+    // the current player
+  }
+
+  isWinnerMove() {
+
+    if(this.addCurrentPlayerMoves().length < 3) {
+      return false;
+      // if has less than 3 moves there are no winning combos
+
+    } else if {
+      // go to the wiiningMoves array and loop into it and find each movecombos
+      // in each move combos i need to verify if my move is part of this combo( loop into the i)
+      // if i have 3 moves that are part of this combo
+      // i have a winner Combo.
+      for(var i = 0; i < this.winningMoves.legnth; i++) {
+        var winninComb = 0;
+        // each this.winningMoves[i] = 3 moves;
+        var moves = this.winningMoves[i];
+
+        for(var m = 0; m < moves.length; m++) {
+          if(this.addCurrentPlayerMoves().includes(moves[m])) {
+            winninComb++;
+            if(winninComb === 3) {
+              // we have a winner!
+              return true;
+            }
+          }
+
+        }
+      }
+      return false;
+
+    }
+
   }
 
 
