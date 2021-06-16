d9ed0623ba2207c8f7f8ff308cfa8116f0391a4e
Joana Brito <joanafiorentini@hotmail.com>
2021-06-15 17:38:18 -0700

    Fix methods

--- game.js
+++ game.js
@@ -3,14 +3,14 @@ class Game {
     this.playerJedi = new Player("jedi","assets/logo_rebel.svg");
     this.playerSith = new Player("sith","assets/empire_logo.svg");
     this.currentPlayer = null;
-    // this.currentPlayer = this.playerJedi;
+    this.winner = null;
     this.board = new Array(9);
     this.winningMoves = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
                          [0, 3, 6], [1, 4, 7], [2, 5, 8],
                          [2, 4, 6], [0, 4, 8]];
   }
 
-  getCurrentPlayer() {
+  changePlayer() {
     if(this.currentPlayer === this.playerJedi) {
       this.currentPlayer = this.playerSith;
     } else {
@@ -19,62 +19,96 @@ class Game {
   }
 
   runPlayerClick(clickedBtnIndex) {
-    // e nesse index vai ser passado o valor do current player name?
-    this.board[clickedBtnIndex] = this.currentPlayer.name;
+    this.board[clickedBtnIndex] = this.currentPlayer.id;
+    this.currentPlayer.moves.push(clickedBtnIndex);
 
-    this.current = this.getCurrentPlayer();
-  }
-
-  addCurrentPlayerMoves() {
-    var currentPlayerMoves = [];
-
-    for(var i = 0; i < this.board.length; i++) {
-      var playerMove = this.board[i];
-
-      if(playerMove === this.currentPlayer.name) {
-        currentPlayerMoves.push(i);
-      }
+    if(this.isWinnerMove()) {
+      console.log("aqui");
+      this.winner = this.currentPlayer;
+      return true;
+    } else if (!this.board.includes(undefined)) {
+      this.winner = false;
+      return ;
     }
-    return currentPlayerMoves;
-    // is returning one array with the moves of
-    // the current player
+
   }
+  //
+  // addCurrentPlayerMoves() {
+  //   // var currentPlayerMoves = [];
+  //
+  //   for(var i = 0; i < this.board.length; i++) {
+  //     var playerMove = this.board[i];
+  //     // console.log(playerMove);
+  //
+  //     if(playerMove === this.currentPlayer.id) {
+  //       this.currentPlayer.moves.push(i)
+  //     }
+  //
+  //     // var isWinningMove = game.isWinnerMove();
+  //     // if(isWinningMove === true ) {
+  //     //   winningMessage.innerText = `${this.currentPlayer.id} won  !!`
+  //     //   if(this.currentPlayer.id === "jedi") {
+  //     //     this.currentPlayer.addWin(this.board);
+  //     //     jediNumberWins.innerText = `“do. or do not. there is no try.” jedi wins: ${this.currentPlayer.numberWins}`;
+  //     //     // console.log("aqui");
+  //     //   } else {
+  //     //     this.currentPlayer.addWin(this.board);
+  //     //     sithNumberWins.innerText = `"this will be a day long remembered" dark side wins: ${this.currentPlayer.numberWins}`;
+  //     //   }
+  //       game.startNewGame();
+  //     }
+  //   }
+  //   // return currentPlayerMoves;
+  // }
 
   isWinnerMove() {
-
-    if(this.addCurrentPlayerMoves().length < 3) {
+    if (this.currentPlayer.moves.length < 3) {
       return false;
     }
 
-    for(var i = 0; i < this.winningMoves.legnth; i++) {
-        var winninComb = 0;
-        // each this.winningMoves[i] = 3 moves;
+    for(var i = 0; i < this.winningMoves.length; i++) {
+        var winningCombo = 0;
         var moves = this.winningMoves[i];
 
         for(var m = 0; m < moves.length; m++) {
-          if(this.addCurrentPlayerMoves().includes(moves[m])) {
-            winninComb++;
-
-            if(winninComb === 3) {
-              // we have a winner!
+          if(this.currentPlayer.moves.includes(moves[m])) {
+            winningCombo++;
+            if(winningCombo === 3) {
               return true;
             }
           }
 
         }
-      }
-
-      return false;
 
     }
+      // // console.log(this.board);
+      // var emptySpaces = 0;
+      // for(var b = 0; b < this.board.length; b++) {
+      //   // console.log(this.board[b]);
+      //   if(this.board[b]=== undefined) {
+      //     emptySpaces++;
+      //   }
+      // }
+      // // console.log(emptySpaces);
+      // if(emptySpaces < 1) {
+      //   return  winningMessage.innerText = `it's a draw  !!`;
+      // }
+
+    return false;
+  }
 
     startNewGame() {
-      if (this.winner) {
-        this.currentPlayer.addWin(this.board);
-        this.currentPlayer.saveWinsToStorage();
-      }
+      // console.log("lalalal")
+      // if (this.winner) {
+      //   // this.currentPlayer.addWin(this.board);
+      //   // this.currentPlayer.saveWinsToStorage();
+      // }
+
       this.board = new Array(9);
-      this.getCurrentPlayer();
+      this.winner = null;
+      this.playerJedi.moves = [];
+      this.playerSith.moves = [];
+      this.changePlayer();
 
 
   // this.setFirstPlayer();
@@ -82,88 +116,3 @@ class Game {
     }
 
   }
-
-// A way to keep track of the data for the game board ( this.board)
-// A way to keep track of which player’s turn it currently is (this.currentPlayer)
-// ****as soon as the click happen change the player.***
-// A way to check the Game’s board data for win conditions
-// A way to detect when a game is a draw (no one has won)
-// // // A way to reset the Game’s board to begin a new game
-// This workflow is not required, but will help you meet the overall requirements of the project.
-//
-//
-// function createNewIdea() {
-//   if (checkInputs() === false) {
-//     return false;
-//   }
-// function displayIdeas() {
-//   titleInput.value = null;
-//   bodyInput.value = null;
-//   starredIdeasBtn.innerText = "Show Starred Ideas";
-//
-//   ideaContainerSection.innerHTML = "";
-//
-//   for (i = 0; i < savedIdeas.length; i++) {
-//     if (savedIdeas[i].star === true) {
-//       ideaContainerSection.innerHTML +=`<article id=${savedIdeas[i].id} class="starred">
-//         <header>
-//             <button id="favorite" class="favorite-button">
-//               <img name="star-white" id="starWhite" src="assets/star.svg" alt="star">
-//               <img name="star-red" id="starRed" src="assets/star-active.svg" alt="star">
-//             </button>
-//             <button id="deleteCard" class="delete-button">
-//               <img name="x-white" src="assets/delete.svg" alt="X">
-//               <img name="x-red" id="x-red" src="assets/delete-active.svg" alt="X">
-//             </button>
-//         </header>
-//         <div class="idea-body">
-//           <strong>${savedIdeas[i].title}</strong>
-//           <p>${savedIdeas[i].body}</p>
-//         </div>
-//         <footer>
-//           <button class="comment-button">
-//             <img src="assets/comment.svg" alt="comment">
-//             <span>Comment</span>
-//           </button>
-//         </footer>
-//       </article>`
-//     } else if (savedIdeas[i].star === false) {
-//       ideaContainerSection.innerHTML += `<article id=${savedIdeas[i].id}>
-//         <header>
-//             <button id="favorite" class="favorite-button">
-//               <img name="star-white" id="starWhite" src="assets/star.svg" alt="star">
-//               <img name="star-red" id="starRed" src="assets/star-active.svg" alt="star">
-//             </button>
-//             <button id="deleteCard" class="delete-button">
-//               <img name="x-white" src="assets/delete.svg" alt="X">
-//               <img name="x-red" id="x-red" src="assets/delete-active.svg" alt="X">
-//             </button>
-//         </header>
-//         <div class="idea-body">
-//           <strong>${savedIdeas[i].title}</strong>
-//           <p>${savedIdeas[i].body}</p>
-//         </div>
-//         <footer>
-//           <button class="comment-button">
-//             <img src="assets/comment.svg" alt="comment">
-//             <span>Comment</span>
-//           </button>
-//         </footer>
-//       </article>`
-//     }
-//   }
-// }
-//     var newIdea = new Idea(titleInput.value, bodyInput.value)
-//     savedIdeas.push(newIdea);
-//     newIdea.saveToStorage();
-//     displayIdeas();
-//   }
-// Plan out the HTML layout (colors and icons do not need to match, but overall layout should closely match the demo video)
-// Create the Player class
-// Create the Game class
-// Make game fully playable without the DOM (manually updating the Game.board data, etc, from your console) to force yourself to think data-model-first
-// Create central game board on the DOM
-// Connect Game data model to the DOM
-// Display the Player data in the sidebars
-// Automatically reset the game board to allow for a new game to be played after the previous game is won
-// Persist Player data using local storage (number of wins should persist across page refreshes)
