1a82b1ee48d5b3efed1da87d3e08b88faeb44b5a
Joana Brito <joanafiorentini@hotmail.com>
2021-06-15 17:40:52 -0700

    Finish class

--- player.js
+++ player.js
@@ -2,18 +2,23 @@ class Player {
   constructor(infoplayer, infoToken) {
     this.id = infoplayer;
     this.token = infoToken;
+    this.moves = [];
     // this.token = `assets/${infoToken}.svg`;
     // this.wins = retrieveWinsFromStorage() || [];
-    this.numberWins = 0;
-    // this.wins = [];
+    // this.numberWins = 0;
+    this.wins = [];
+    this.retrieveWinsFromStorage();
+
   }
   addWin(winnerBoard) {
     this.wins.push(winnerBoard);
-    this.numberWins++;
+    this.saveWinsToStorage();
+    // this.numberWins++;
+    // this.numberwins = this.wins.leght
 
   }
   saveWinsToStorage() {
-    var saveWins = JSON.stringify(this);
+    var saveWins = JSON.stringify(this.wins);
     // I need to get from local storage per player ( id?) each wins
   localStorage.setItem(`${this.id}`, saveWins);
 
@@ -22,7 +27,8 @@ class Player {
     // do i need to instanciate again? loop and instaciate? or just get only the info from wins
 
     var retriveWins = localStorage.getItem(`${this.id}`);
-    this.wins = JSON.parse(retriveWins[this.wins][this.numberWins]);
+    this.wins = JSON.parse(retriveWins)|| [];
+
     // do i need to return anythig?
 
   }
