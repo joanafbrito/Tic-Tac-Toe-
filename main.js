9a57915624ac837dc0108951df3f137a7aea2283
Joana Brito <joanafiorentini@hotmail.com>
2021-06-15 17:43:15 -0700

    Add methods to the main

--- main.js
+++ main.js
@@ -1,23 +1,18 @@
-// file that contains all DOM related JavaScript
-// This workflow is not required, but will help you meet the overall requirements of the project.
-//
-// Plan out the HTML layout (colors and icons do not need to match, but overall layout should closely match the demo video)
-// Make game fully playable without the DOM (manually updating the Game.board data, etc, from your console) to force yourself to think data-model-first
-// Create central game board on the DOM
-// Connect Game data model to the DOM
-// Display the Player data in the sidebars
-// Automatically reset the game board to allow for a new game to be played after the previous game is won
-// Persist Player data using local storage (number of wins should persist across page refreshes)
-var buttons = document.querySelectorAll(".buttons");
+var buttons = document.querySelectorAll(".buttons button");
 var playerTurn = document.getElementById("playerTurn");
-// var btns = Array.from(buttons);- option but only for modern browsers.
+// var boardMessage = document.querySelector(".game-board");
+var jediNumberWins = document.getElementById("jediWins");
+var sithNumberWins = document.getElementById("sithWins");
 // --change the node list into one array.
 var btns = Array.prototype.slice.call(buttons);
-// var token =
+
 
 var game = new Game();
 
 game.startNewGame();
+jediNumberWins.innerHTML = `“do. or do not. there is no try.” <br> jedi wins: ${game.playerJedi.wins.length}`;
+sithNumberWins.innerHTML = `"this will be a day long remembered" <br> dark side wins: ${game.playerSith.wins.length}`;
+
 
 
 for (var i = 0; i < btns.length; i++) {
@@ -26,49 +21,50 @@ for (var i = 0; i < btns.length; i++) {
 
   button.addEventListener("click",function(event) {
     event.preventDefault();
+    console.log(game.winner);
+    if (game.winner !== null) {
+      console.log("heloo");
+      game.startNewGame();
+      playerTurn.innerHTML = `May the Force be with you, it's <strong class="${game.currentPlayer.id}">${game.currentPlayer.id}</strong>'s turn!`;
 
-// se no momento no click ou evento eu target o conteudo do html e ele for vazio, ele deve adicionar o token do
-// current player , se for diferente de vazio( ou se tiver algum conteudo, pare e nao altere nada.- talvez possa retornar nada)
+      for(var b = 0; b < btns.length; b++){
+        btns[b].innerHTML = "";
+      }
+      return;
+    }
     if(event.target.innerHTML !== "") {
       return false;
     } else {
       event.target.innerHTML = `<img src="${game.currentPlayer.token}" alt="${game.currentPlayer.id}"/>`;
-      // pra identificar o local que foi clicado eu pego o index do botao clicado.
       var clikedBtnIndex = btns.indexOf(event.target);
-
-// and add the current player to the game using the method created on the class.
       game.runPlayerClick(clikedBtnIndex);
-      playerTurn.innerText = `May the Force be with you, it's ${game.currentPlayer.id}'s turn!`;
+
+      if(game.winner === false) {
+        playerTurn.innerText = `it's a draw  !!`
+      } else if (game.winner === null) {
+        game.changePlayer()
+        playerTurn.innerHTML = `May the Force be with you, it's <strong class="${game.currentPlayer.id}">${game.currentPlayer.id}</strong>'s turn!`;
+
+      }else {
+        console.log("victory");
+          playerTurn.innerText = `${game.currentPlayer.id} won  !!`
+          game.currentPlayer.addWin(game.board);
+          if(game.currentPlayer.id === "jedi") {
+
+            jediNumberWins.innerHTML = `“do. or do not. there is no try.” <br> jedi wins: ${game.currentPlayer.wins.length}`;
+
+          }else {
+            sithNumberWins.innerHTML = `"this will be a day long remembered" <br> dark side wins: ${game.currentPlayer.wins.length}`;
+          }
+
+      }
+
+
     }
 
-    // console.log(event.target.getAttribute("id"));
-    var btnIndex = parseInt(event.target.getAttribute("id").replace("btn",""));
-    if(btnIndex === btns[i]) {
-
-    }
-    // The target event property returns the element that triggered the event.
-    // The target property gets the element on which the event originally occurred,
-    // opposed to the currentTarget property, which always refers to the element whose
-    // event listener triggered the event
-    // Events are objects with certain properties, and e. target almost always represents a DOM element.
-    // Thus e. target. value is the value property of some DOM element, in this case that means the text entered
-    // in the search input.
-    // I'm tageting the id
-    // console.log (index);
-    // gameOne.board[index]=
+    // var btnIndex = parseInt(event.target.getAttribute("id").replace("btn",""));
+    // if(btnIndex === btns[i]) {
+    // }
 // gameOne.runPlayerClick(1);
   });
 }
-
-// gameOne.runPlayerClick(1);
-
-// var winner- pseudocode.
-// if(playerOne.token === [0,4,8] || playerOne.token === [2,4,6] ||
-//    playerOne.token === [1,4,7] || playerOne.token === [3,4,5] ||
-//    playerOne.token === [6,7,8] || playerOne.token === [0,1,2] ||
-//    playerOne.token === [0,3,6] || playerOne.token ===[2,5,8]) {
-//
-//      return `${playerOne} is the Winner!`
-//    } else {
-//      gameOne.getCurrentPlayer();
-//    }
